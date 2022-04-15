31a07c6b99c490f635470d3ef10bea4fa7fb8c79
lordsainath1 <saipm2003@gmail.com>
2022-04-15 19:22:42 +0530

    change1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Contact from "./components/Center/Contact";
+import Content from "./components/Center/Content";
+import Hero from "./components/Center/Hero";
+import Footer from "./components/Footer/Footer";
+import Navigation from "./components/Header/Navigation";
+
+
 
 function App() {
   return (
     <>
-    
+    <Navigation/>
+    <Hero/>
     <div className='text-red-400 text-6xl flex justify-center items-center'>
       Hello Sainath 
     </div>
+
+<Content/>
+
+<Contact/>
+
+    <Footer/>
+
     
     </>
   );
