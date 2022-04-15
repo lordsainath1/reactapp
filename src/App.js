96d3ae3a1eff88b0c13d8f5dc3cce62978101414
lordsainath1 <saipm2003@gmail.com>
2022-04-15 17:18:12 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+    <div className='text-red-400 text-6xl flex justify-center items-center'>
+      Hello Sainath 
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
